dc059e8e6fbff84f2a9bd35b80e3f184c054d083
insane-beaver <OlexandrVaskov@gmail.com>
2021-01-13 14:50:06 +0200

    demo style

--- notes-angular/src/app/app-routing.module.ts
+++ notes-angular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NotesListComponent} from "./pages/notes-list/notes-list.component";
+import {MainLayoutComponent} from "./pages/main-layout/main-layout.component";
+import {OptionsComponent} from "./pages/options/options.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainLayoutComponent, children: [
+      { path: '', component: NotesListComponent },
+      { path: 'options', component: OptionsComponent }
+    ] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- notes-angular/src/app/app.module.ts
+++ notes-angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotesListComponent } from './pages/notes-list/notes-list.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { OptionsComponent } from './pages/options/options.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesListComponent,
+    MainLayoutComponent,
+    OptionsComponent
   ],
   imports: [
     BrowserModule,
