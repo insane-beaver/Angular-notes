e988af7b9e292015f8c1c18bc05790e2664dde39
insane-beaver <OlexandrVaskov@gmail.com>
2021-01-15 13:47:45 +0200

    Styles added

--- notes-angular/src/app/app.module.ts
+++ notes-angular/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NotesListComponent } from './pages/notes-list/notes-list.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 import { OptionsComponent } from './pages/options/options.component';
+import { NoteCardComponent } from './note-card/note-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotesListComponent,
     MainLayoutComponent,
-    OptionsComponent
+    OptionsComponent,
+    NoteCardComponent
   ],
   imports: [
     BrowserModule,
--- notes-angular/src/app/pages/notes-list/notes-list.component.ts
+++ notes-angular/src/app/pages/notes-list/notes-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, Input, OnInit, Renderer2, ViewChild, ViewChildren} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-notes-list',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesListComponent implements OnInit {
 
-  constructor() { }
+  /*@ViewChild('truncator') truncator: ElementRef<HTMLElement>;
+  @ViewChild('bodyText') bodyText: ElementRef<HTMLElement>;*/
+
+  @ViewChild('truncator') truncator = document.getElementById('truncator');
+  @ViewChild('bodyText') bodyText = document.getElementById('bodyText');
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    // @ts-ignore
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewableHeight = parseInt(style.getPropertyValue('height'), 10);
+
+    // @ts-ignore
+    if(this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+      // @ts-ignore
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block'); //no overflow
+    } else {
+      // @ts-ignore
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none'); //overflow
+    }
+
   }
 
 }
